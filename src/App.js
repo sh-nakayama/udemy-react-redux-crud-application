3e6d1352403e8a2448174b0915d8d3d696b072ab
MAC <>
2018-12-04 21:45:40 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
 import React from 'react';
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <label htmlFor="bar">bar</label>
-//         <input type="text" onChange={()=>{console.log("I am clicked!")}}/>
-//       </div>
-//     )
-//   }
-// }
-
 const App = () => {
+  const profiles=[
+    {name:"Taro",age:14},
+    {name:"Hanako",age:17},
+    {name:"Noname"}
+  ]
   return(
     <div>
-    <Cat></Cat>
-    <Cat></Cat>
-    <Cat></Cat>
-    </div>
+    {
+      profiles.map((profile,index) =>{
+        return <User name={profile.name} age={profile.age} key={index}></User>
+      }
+      )
+    }
+  </div>
   )
 }
 
-const Cat = () => {
+const User = (props) => {
   return(
     <div>
-    Meow!
+    Hi I am {props.name}, and {props.age} years old.
     </div>
 
   )
 }
 
+User.defaultProps = {
+  age: 1
+}
 
 export default App;
